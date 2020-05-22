b97d9e48d321750afd3da1b44cd858067516c60a
Grenville-PROLINK <63523665+Grenville-PROLINK@users.noreply.github.com>
2020-05-21 20:21:05 -0400

    Java API - POC for Azure Spring Cloud deployment

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO beer(name,abv) VALUES ('Stella Artois', 5.0);
+INSERT INTO beer(name,abv) VALUES ('Czechvar', 2.0);
+INSERT INTO beer(name,abv) VALUES ('Golden Pheasant', 4.0);
+
+COMMIT;
